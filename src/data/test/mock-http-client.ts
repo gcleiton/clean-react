26cffff066e8f09c2d887adae2d623f8a37f697b
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-12-03 21:38:23 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,12 @@
-import { HttpPostParams } from './../protocols/http/http-post-client'
-import { HttpPostClient } from 'data/protocols/http/http-post-client'
+import { HttpPostParams, HttpPostClient } from '../protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements HttpPostClient {
     url?: string
+    body?: object
 
     async post (params: HttpPostParams): Promise<void> {
         this.url = params.url
+        this.body = params.body
         return Promise.resolve()
     }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { mockAuthentication } from './../../../domain/test/mock-authentication'
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import faker from 'faker'
@@ -21,7 +22,14 @@ describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
         const url = faker.internet.url()
         const { sut, httpPostClientSpy } = makeSut(url)
-        await sut.auth()
+        await sut.auth(mockAuthentication())
         expect(httpPostClientSpy.url).toBe(url)
+    })
+
+    test('Should call HttpPostClient with correct body', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const authenticationParams = mockAuthentication()
+        await sut.auth(authenticationParams)
+        expect(httpPostClientSpy.body).toEqual(authenticationParams)
     })
 })
